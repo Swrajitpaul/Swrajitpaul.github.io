0626ed007d37ca8cfd0b32a7bed1cd4f621190a7
Swrajit Paul <swrajitpaul@gmail.com>
2022-12-29 14:54:10 -0500

    v11.0
    Releasing version 110

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+setTimeout(headertwo, 800)
+setTimeout(headerthree, 900)
+setTimeout(letter, 1100)
+setTimeout(lettergray, 1200)
+setTimeout(letter, 1300)
+function letter() {
+    document.getElementById("letter").style.color = "salmon"
+}
 
+function lettergray() {
+    document.getElementById("letter").style.color = "gray"
+}
+
+function headertwo() {
+    document.getElementById("headertwo").style.color = "salmon"
+}
+
+function headerthree() {
+    document.getElementById("headerthree").style.color = "salmon"
+}
