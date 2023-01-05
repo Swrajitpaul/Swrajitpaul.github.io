a133c38a99f6fc907fad75a15a8afc55cc429e09
Swrajit Paul <swrajitpaul@gmail.com>
2023-01-04 22:58:22 -0500

    Refactoring index files

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-setTimeout(headertwo, 800)
-setTimeout(headerthree, 1500)
-setTimeout(lettergray, 50)
-setTimeout(navigation, 100)
-
-function letter() {
-    document.getElementById("letter").style.color = "salmon"
-}
-
-function lettergray() {
-    $('#letter').delay(1700).fadeIn(100).fadeOut(100).fadeIn(150).fadeOut(150).fadeIn(200);
-}
-
-function headertwo() {
-    document.getElementById("headertwo").style.color = "salmon"
-}
-
-function headerthree() {
-    document.getElementById("headerthree").style.color = "salmon"
-}
-
-function navigation() {
-    $("#navigation").delay(2000).slideDown(2000);
-    $("footer").delay(3000).slideDown(2000);
-}
+$(document).ready(function () {
+    $('#headertwo').delay(800).fadeIn();
+    $('#headerthree').delay(1500).fadeIn();
+    $('#letter').delay(1750).fadeIn(100).fadeOut(100).fadeIn(150).fadeOut(150).fadeIn(200);
+    $('#navigation').delay(2000).slideDown(1000);
+    $('#footer').delay(2500).slideDown(1000);
+});
