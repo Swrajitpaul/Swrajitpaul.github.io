d07b402c8e3f478992f7243aa0cc9c4627ecc8b6
Swrajit Paul <swrajitpaul@gmail.com>
2023-01-05 03:00:11 -0500

    Modifying animation

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     $('#headertwo').delay(800).fadeIn();
     $('#headerthree').delay(1500).fadeIn();
     $('#letter').delay(1750).fadeIn(100).fadeOut(100).fadeIn(150).fadeOut(150).fadeIn(200);
-    $('#navigation').delay(2000).slideDown(1000);
-    $('#footer').delay(2500).slideDown(1000);
+    $('#navigation').delay(2500).fadeIn(1000);
+    $('#footer').delay(3000).fadeIn(1000);
 });
