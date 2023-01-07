cf963756f8fdbf6cc3ff365e5f8cd934227600a2
Swrajit Paul <swrajitpaul@gmail.com>
2023-01-06 21:45:46 -0500

    12.2
    Adding animation

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ $(document).ready(function () {
     $('#headerthree').delay(1500).fadeIn();
     $('#letter').delay(1750).fadeIn(100).fadeOut(100).fadeIn(150).fadeOut(150).fadeIn(200);
     $('#navigation').delay(2500).fadeIn(1000);
-    $('#footer').delay(3000).fadeIn(1000);
+    $('#footer').delay(2500).fadeIn(1000);
 });
